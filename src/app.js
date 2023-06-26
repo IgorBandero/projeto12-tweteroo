03907475c31c1e745e508ffc418d261037b5b759
Igor Bandero <igorbandero@gmail.com>
2023-06-26 15:39:27 -0300

    POST /tweets

--- src/app.js
+++ src/app.js
@@ -7,11 +7,19 @@ app.use(express.json())
 app.use(cors());
 
 let users = [];
-
 let tweets = [];
 
+function userIsValid(name){
+	for (let i=0; i<users.length; i++){
+		if (name === users[i].username){
+			return true;
+		}
+	}
+	return false;
+}
+
 app.post("/sign-up", (req, res) => {
-	const {username, avatar} = req.params;
+	const {username, avatar} = req.body;
 	const newUser = {
 		username,
 		avatar
@@ -19,6 +27,23 @@ app.post("/sign-up", (req, res) => {
 	users.push(newUser);
 	res.send("OK");
 })
+
+app.post("/tweets", (req, res) => {
+	const {username, tweet} = req.body;
+
+	if(userIsValid(username)){
+		const newTweet = {
+			username,
+			tweet
+		};
+		tweets.push(newTweet);
+		res.send("OK")
+	}
+	else {
+		res.send("UNAUTHORIZED");
+	}
+})
+
 
 // Inicia o servidor na porta 5000
 app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
